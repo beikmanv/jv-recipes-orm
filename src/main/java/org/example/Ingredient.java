488a53e4ece9884c729a0eb9e3c151fd57817442
Valdis <v.beikmanis@gmail.com>
2024-11-22 11:53:05 +0000

    feat: Category and Tag classes

---
+++ src/main/java/org/example/Ingredient.java
@@ -0,0 +1,8 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Ingredient {
+
+}
---
+++ src/main/java/org/example/Tag.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+
+import javax.annotation.processing.Generated;
+
+@Entity
+class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int iD;
+
+    @NotNull
+    private String name;
+
+    public int getiD() {
+        return iD;
+    }
+
+    public @NotNull String getName() {
+        return name;
+    }
+
+    public void setiD(int iD) {
+        this.iD = iD;
+    }
+
+    public void setName(@NotNull String name) {
+        this.name = name;
+    }
+
+    public Tag() {}
+}
